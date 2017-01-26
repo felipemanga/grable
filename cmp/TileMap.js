b92a8a0272f11fb24939b892d17381256dd6c635
FManga <lufeboma@gmail.com>
2017-01-26 00:58:48 -0300

    isometric map support

--- cmp/TileMap.js
+++ cmp/TileMap.js
@@ -5,25 +5,41 @@ CLAZZ("cmp.TileMap", {
     CONSTRUCTOR:function(){
         var json = this.game.cache.getTilemapData(this.asset);
         var mat = new PIXI.Matrix(), edef = this.gameState.entityDefinitions;
-        var image, imageframe, gidcache = {};
+        var image, imageframe, gidcache = {}, isometric = json.data.orientation == "isometric";
+        var tileheight = json.data.tileheight, tilewidth = json.data.tilewidth;
+        var halfWorldWidth = tilewidth * json.data.width * 0.5, halfWorldHeight = tileheight * json.data.height * 0.5;
 
         for( var layerId=0; layerId<json.data.layers.length; ++layerId ){
             var layer = json.data.layers[layerId];
-            if( layer.type == "tilelayer" ) addTileLayer.call(this, layer);
-            if( layer.type == "objectgroup" ) addObjectGroup.call(this, layer);
+            if( !layer.visible ) continue;
+            var data = layer.data;
+            if( layer.encoding == "base64" ){
+                data = atob(data);
+                var buf = new Uint8Array(data.length);
+                for( var i=0, l=data.length; i<l; ++i )
+                    buf[i] = data.charCodeAt(i);
+                data = new Uint32Array(buf.buffer);
+            }
+
+            if( layer.type == "tilelayer" ) addTileLayer.call(this, layer, data);
+            if( layer.type == "objectgroup" ) addObjectGroup.call(this, layer, data);
         }
 
-        function addTileLayer(layer){
+        function addTileLayer(layer, data){
             var x = 0,
                 y = 0,
-                map = json.data,
-                obj = {x:0, y:0};
-            for (var i = 0, ldl=layer.data.length; i < ldl; ++i) {
-                var o = layer.data[i];
+                obj = {x:0, y:0, width:tilewidth, height:tileheight};
+            for (var i = 0, ldl=data.length; i < ldl; ++i) {
+                var o = data[i];
                 if (o) {
-                    obj.x = x * map.tilewidth  + layer.x;
-                    obj.y = y * map.tileheight + layer.y;
-                    instanceDO.call(this, o, obj);
+                    if( isometric ){
+                        obj.x = (x-y) * tilewidth*0.5  + layer.x;
+                        obj.y = (x+y) * tileheight*0.5 + tileheight + layer.y;
+                    }else{
+                        obj.x = x * tilewidth  + layer.x;
+                        obj.y = y * tileheight + layer.y;
+                    }
+                    instanceDO.call(this, o, obj, true);
                 }
                 x++;
                 if (x == layer.width) {
@@ -36,11 +52,21 @@ CLAZZ("cmp.TileMap", {
         function addObjectGroup(layer){
             for( var objectId=0; objectId<layer.objects.length; ++objectId ){
                 var obj = layer.objects[objectId];
-                instanceDO.call(this, obj.gid, obj);
+                if( isometric ){
+                    var tx = obj.x / tileheight; // not a typo.
+                    var ty = obj.y / tileheight;
+                    obj = DOC.merge(obj, {
+                        x: (tx - ty)*tilewidth*0.5 + layer.x,
+                        y: (tx + ty)*tileheight*0.5 + layer.y
+                    });
+                }
+                var DO = instanceDO.call(this, obj.gid, obj, isometric);
+                if( "width" in obj ) DO.width = obj.width;
+                if( "height" in obj ) DO.height = obj.height;
             }
         }
 
-        function instanceDO(gid, obj){
+        function instanceDO(gid, obj, adjust){
             var DO, rotation = obj.rotation||0;
             resolveGID(gid);
 
@@ -56,9 +82,22 @@ CLAZZ("cmp.TileMap", {
             }else{
                 DO = this.game.add.image(0,0,image,imageframe);
             }
-            mat.identity().translate( DO.width * DO.anchor.x, DO.height * DO.anchor.y - DO.height).rotate(rotation).translate( obj.x || 0, obj.y || 0 );
-            DO.position.x = mat.tx;
-            DO.position.y = mat.ty;
+            var x = obj.x || 0
+                y = obj.y || 0;
+
+            if( isometric ){
+                x += json.data.height*tilewidth*0.5;
+                y += tileheight*2;
+            }
+
+            DO.position.x = x;
+            DO.position.y = y;
+
+            if( isometric ) DO.anchor.x = 0.5;
+            else DO.anchor.x = 0;
+
+            DO.anchor.y = 1;
+            DO.rotation = rotation;
             return DO;
         }
 

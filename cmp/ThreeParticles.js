38e8fe3777c5d40308f505d5212293f72f32d9ff
FManga <lufeboma@gmail.com>
2017-02-27 16:43:22 -0300

    particles

--- cmp/ThreeParticles.js
+++ cmp/ThreeParticles.js
@@ -36,7 +36,7 @@ CLAZZ("cmp.ThreeParticles", {
 });
 
 CLAZZ("cmp.ThreeParticles.Server", {
-    INJECT:['pool', 'scene'],
+    INJECT:['pool', 'scene', 'game'],
 
     index:null,
 
@@ -75,7 +75,11 @@ uniform float opacity;
 #include <common>
 #include <packing>
 #include <color_pars_fragment>
-#include <map_particle_pars_fragment>
+
+uniform vec4 offsetRepeat;
+uniform sampler2D map;
+// # include <map_particle_pars_fragment>
+
 #include <fog_pars_fragment>
 #include <shadowmap_pars_fragment>
 #include <logdepthbuf_pars_fragment>
@@ -85,10 +89,17 @@ void main() {
 
 	#include <clipping_planes_fragment>
 
-	vec3 outgoingLight = vec3( 0.0 );
+	vec3 outgoingLight = vec3( 1.0 );
 	vec4 diffuseColor = vec4( diffuse, opacity );
 
 	#include <logdepthbuf_fragment>
+    vec4 mapTexel = texture2D( map, 
+        vec2( gl_PointCoord.x, 1.0 - gl_PointCoord.y )
+        * offsetRepeat.zw 
+        + offsetRepeat.xy );
+
+    diffuseColor *= mapTexelToLinear( mapTexel );
+
 	#include <map_particle_fragment>
 	#include <color_fragment>
 	#include <alphatest_fragment>
@@ -139,13 +150,14 @@ void main() {
         geometry.addAttribute("position", position);
         geometry.drawRange.count = 0;
 
+        position.array[0] = 500; // Necessary. I don't know why.
+
         return geometry;
     },
 
     getMaterial:function( texture ){
-        // var mat = new THREE.PointsMaterial( { color:0xffffff, size:20 } );
+        // var mat = new THREE.PointsMaterial( { color:0xffffff, size:100, opacity:1 } );
         // mat.map = (new THREE.TextureLoader()).load(texture);
-        // mat.transparent = true;
         // mat.blending = THREE.AdditiveBlending;
         // mat.depthWrite = false;
         // mat.transparent = true;
@@ -156,15 +168,22 @@ void main() {
             vertexShader: this.vertexShader,
             uniforms: THREE.UniformsUtils.merge( [
 				THREE.UniformsLib.points,
-				THREE.UniformsLib.fog
-                // ,
-                // {
-                //     time: {value:0}                    
-                // }
+				THREE.UniformsLib.fog,
+                {
+                    time: {value:0},
+                }
 			] )
         });
-        mat.uniforms.size.value = 20;
+
         mat.uniforms.map.value = (new THREE.TextureLoader()).load(texture);
+        
+        mat.uniforms.size.value = 60;
+        // mat.uniforms.opacity = 1;
+        // mat.uniforms.diffuse.value = new THREE.Color(0xFFFFFF);
+        // mat.uniforms.offsetRepeat.value.set( 0, 0, 1, 1 );
+
+        mat.blending = THREE.AdditiveBlending;
+        mat.transparent = true;
         return mat;
     },
 
@@ -183,15 +202,23 @@ void main() {
         this.emitters.splice( pos, 1 );
     },
 
+    particleCount:0,
     onTick:function(delta){
-        for( var k in this.index ){
+    	if( delta < 0 )
+    		return;
 
+        var scale = this.game.height / this.game.camera.aspect;
+    		
+        for( var k in this.index ){
+            var dirty = false;
             var index = this.index[k];
             var emitters = index.emitters;
             var geometry = index.geometry;
             var material = index.material;
             var position = geometry.attributes.position.array;
-            // material.uniforms.time.value += delta;
+            material.uniforms.time.value += delta;
+            material.uniforms.scale.value = scale;
+
 
             for( var i=0, l=emitters.length; i<l; ++i ){
 
@@ -216,9 +243,11 @@ void main() {
                     position[p++] = pos.z;
                 }
 
-                if( acc )
-                    geometry.attributes.position.needsUpdate = true;
+                if( acc > 0 )
+                    dirty = true;
             }
+            if( dirty )
+                geometry.attributes.position.needsUpdate = true;
         }
     },
     
@@ -238,7 +267,8 @@ void main() {
             if( !instance ){
                 instance = CLAZZ.get( cmp.ThreeParticles.Server, {
                     pool:emitter.entity.pool,
-                    scene:emitter.game.scene
+                    scene:emitter.game.scene,
+                    game:emitter.game
                 });
             }
 			
